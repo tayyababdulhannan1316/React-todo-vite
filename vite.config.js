f08d326ac70439dbc06f7d027183e6a1c81e5cf1
Tayyab Abdul Hannan <tayyab.abdulhannan@yahoo.com>
2025-05-28 11:07:28 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      config: path.resolve(__dirname, 'src/config'),
+      contexts: path.resolve(__dirname, 'src/contexts'),
+    },
+  },
+})
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
